c11646b6d603993a4b244e48e838950655c19f3a
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-09 15:22:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+function findMatching(array, string) {
+  return array.filter(function(element) {
+    return element.toLowerCase().startsWith(string.toLowerCase());
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(function(element) {
+    return element.toLowerCase().startsWith(string.toLowerCase());
+  });
+}
+
+function matchName(array, string) {
+  return array.filter(function(element) {
+    return element.name.toLowerCase().startsWith(string.toLowerCase());
+  });
+}
